5140a0f2cda6ef154f17976bb99f7c4cbc98ecc9
Bamlak Gessessew <bamlak@fb.com>
2017-07-11 10:33:54 -0700

    login created

---
+++ Serve/LoginViewController.swift
@@ -0,0 +1,76 @@
+//
+//  LoginViewController.swift
+//  Serve
+//
+//  Created by Bamlak Gessessew on 7/10/17.
+//  Copyright © 2017 Bamlak Gessessew. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    var emptyFieldAlert: UIAlertController!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Set up the emptyFieldAlert
+        emptyFieldAlert = UIAlertController(title: "Empty Field", message: "Fill in all text fields!", preferredStyle: .alert)
+        // create a cancel action
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel) { (action) in
+            // handle cancel response here. Doing nothing will dismiss the view.
+        }
+        // add the cancel action to the alertController
+        emptyFieldAlert.addAction(cancelAction)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPressSignIn(_ sender: Any) {
+        //check to display error message if one of the field is empty
+        if (usernameTextField.text?.isEmpty)! || (passwordTextField.text?.isEmpty)! {
+            present(emptyFieldAlert, animated: true) { }
+            return
+        }
+        
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User log in failed: \(error.localizedDescription)")
+            } else {
+                print("User logged in successfully")
+                // display view controller that needs to shown after successful login
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+
+    @IBAction func didPressSignUp(_ sender: Any) {
+        self.performSegue(withIdentifier: "signupSegue", sender: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Serve/SignupViewController.swift
@@ -0,0 +1,67 @@
+//
+//  SignupViewController.swift
+//  Serve
+//
+//  Created by Bamlak Gessessew on 7/11/17.
+//  Copyright © 2017 Bamlak Gessessew. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignupViewController: UIViewController {
+
+    var emptyFieldAlert: UIAlertController!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPressSignUp(_ sender: Any) {
+        //check to display error message if one of the field is empty
+        if (usernameTextField.text?.isEmpty)! || (passwordTextField.text?.isEmpty)! {
+            present(emptyFieldAlert, animated: true) { }
+            return
+        }
+        
+        let newUser = PFUser()
+        newUser.username = usernameTextField.text
+        newUser.email = emailTextField.text
+        newUser.password = passwordTextField.text
+        
+        newUser.signUpInBackground { (success: Bool, error: Error?) in
+            if success {
+                print("Yay, created a user!")
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print(error?.localizedDescription as Any)
+            }
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
