91978a6245695bee3ba5d5adce668c483f710b21
Michael Hamlett <mhamlett@hmc.edu>
2017-07-17 10:01:11 -0700

    Preliminary work on gathering pending users from event post.

---
+++ Serve/PendingCell.swift
@@ -0,0 +1,26 @@
+//
+//  PendingCell.swift
+//  Serve
+//
+//  Created by Michael Hamlett on 7/13/17.
+//  Copyright © 2017 Bamlak Gessessew. All rights reserved.
+//
+
+import UIKit
+
+class PendingCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
