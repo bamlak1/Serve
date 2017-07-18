031c9a1420f602b05950c94bff174189dc0d09d7
Michael Hamlett <mhamlett@hmc.edu>
2017-07-17 17:15:12 -0700

    Organizations can now see their updates on organization page.

--- Serve/OrganizationViewController.swift
+++ Serve/OrganizationViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import Parse
 
-class OrganizationViewController: UIViewController {
+class OrganizationViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
     
     let user = PFUser.current()!
+    var updates : [PFObject] = []
     
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var bannerImageView: UIImageView!
     @IBOutlet weak var orgNameLabel: UILabel!
     @IBOutlet weak var missionLabel: UILabel!
@@ -23,7 +25,12 @@ class OrganizationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         retrieveOrgData()
+        retrieveOrgUpdates()
 
         // Do any additional setup after loading the view.
     }
@@ -61,6 +68,46 @@ class OrganizationViewController: UIViewController {
         
     }
 
+    
+    func retrieveOrgUpdates() {
+        let query = PFQuery(className: "Post")
+        query.whereKey("org", equalTo: user)
+        query.includeKey("org")
+        query.includeKey("event")
+        
+        
+        query.findObjectsInBackground { (updates: [PFObject]?, error: Error?) in
+            if let updates = updates {
+                self.updates = updates
+                self.tableView.reloadData()
+                
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return updates.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
+        
+        let update = updates[indexPath.row]
+        let org = update["org"] as! PFObject
+        print(org)
+        let event = update["event"] as! PFObject
+        let orgName = org["username"] as! String
+        let eventTitle = event["title"] as! String
+        let action = update["action"] as! String
+        
+        cell.nameLabel.text = orgName
+        cell.actionLabel.text = action
+        cell.eventLabel.text = eventTitle
+        
+        return cell
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
