c7956cae14065e06616d21cae2bd4d250035b896
Michael Hamlett <mhamlett@hmc.edu>
2017-07-31 13:47:10 -0700

    Added causes to browse page.

--- Serve/CauseCell.swift
+++ Serve/CauseCell.swift
@@ -9,12 +9,35 @@
 import UIKit
 import AlamofireImage
 import Parse
-
+import ParseUI
 class CauseCell: UICollectionViewCell {
+    @IBOutlet weak var causeLabel: UILabel!
+    @IBOutlet weak var imageViewer: PFImageView!
+    var pic : PFFile?
+    @IBOutlet weak var button: UIButton!
     
-    @IBOutlet weak var causeName: UILabel!
-    @IBOutlet weak var causeImageView: UIImageView!
-    var cause: PFObject?
+    var indexPath: IndexPath?{
+        didSet{
+            button.tag = (indexPath?.item)!
+        }
+    }
+    
+    var cause: PFObject! {
+        didSet{
+            causeLabel.text = cause["name"] as? String
+            pic = cause["image"] as? PFFile
+            imageViewer.image = nil
+            pic?.getDataInBackground(block: { (data: Data?, error: Error?) in
+                if (error != nil) {
+                    print(error?.localizedDescription ?? "error")
+                } else {
+                    let finalImage = UIImage(data: data!)
+                    self.imageViewer.image = finalImage
+                }
+            })
+            
+        }
+    }
     
     
     
